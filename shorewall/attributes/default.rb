b71298bfbc7478419bf763aee088f11014456a92
Armin Bauer <armin.bauer@amiando.com>
2012-06-12 16:07:58 +0200

    Removed more default attributes

--- shorewall/attributes/default.rb
+++ shorewall/attributes/default.rb
@@ -1,21 +1,20 @@
-
-default[:shorewall][:private_ranges] = ['192.168.0.0/16', '172.16.0.0/12', '10.0.0.0/8']
+default[:shorewall][:private_ranges] = []
 
 default[:shorewall][:enabled] = false
 
-default[:shorewall][:zone_interfaces][:net] = "eth0"
-default[:shorewall][:zone_interfaces][:lan] = "eth0"
-default[:shorewall][:zone_hosts][:lan] = "search:*:*"
-default[:shorewall][:zone_hosts][:net] = "0.0.0.0/0"
+#default[:shorewall][:zone_interfaces][:net] = "eth0"
+#default[:shorewall][:zone_interfaces][:lan] = "eth0"
+#default[:shorewall][:zone_hosts][:lan] = "search:*:*"
+#default[:shorewall][:zone_hosts][:net] = "0.0.0.0/0"
 
 default[:shorewall][:default_interface_settings][:broadcast] = "detect"
 default[:shorewall][:default_interface_settings][:options] = "tcpflags,blacklist,routefilter,nosmurfs,logmartians,dhcp"
 
 # zones ordered from most specific to most general
 override[:shorewall][:zones] = [
-	{ :zone => "fw", :type => "firewall" },
-    { :zone => "lan", :type => "ipv4" },
-    { :zone => "net", :type => "ipv4" }
+#	{ :zone => "fw", :type => "firewall" },
+#    { :zone => "lan", :type => "ipv4" },
+#    { :zone => "net", :type => "ipv4" }
 ]
 
 override[:shorewall][:policy] = [
